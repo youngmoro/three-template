21746c7b2655be48b79795db4307ccb04fcd40f9
youngmoro <morofujix@keio.jp>
2023-03-08 15:04:54 +0900

    add canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { Canvas } from "@react-three/fiber";
 import "./App.css";
 
 function App() {
@@ -7,7 +7,11 @@ function App() {
       <div>
         <img src="/vite.svg" className="logo" alt="Vite logo" />
         <img src="/react.svg" className="logo react" alt="React logo" />
+        <img src="/three.png" className="logo" alt="Three logo" />
         <h2>Vite + React + Three Canvas</h2>
+      </div>
+      <div className="canvas">
+        <Canvas></Canvas>
       </div>
     </div>
   );
