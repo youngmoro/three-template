bfa17b3d0725705963ab6c6173504431c51915a9
youngmoro <morofujix@keio.jp>
2023-03-08 16:22:43 +0900

    add Hooks and gui

--- src/Element.tsx
+++ src/Element.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+import { useThree } from "@react-three/fiber";
+import GUI from "lil-gui";
+import React, { useEffect, useRef, useState } from "react";
+import * as THREE from "three";
 
 const Element = () => {
-  return <div>Element</div>;
+  const { scene, camera, gl } = useThree();
+  const [radius, setRadius] = useState(50);
+
+  const params = {
+    size: radius,
+  };
+
+  const guiRef = useRef<GUI>();
+
+  useEffect(() => {
+    const gui = new GUI();
+    gui
+      .add(params, "size", 0, 100)
+      .onChange((value: number) => setRadius(value));
+    guiRef.current = gui;
+  }, []);
+
+  const box = {
+    const geo = new THREE.BoxBufferGeometry(radius, radius, radius);
+  }
+
+  return <></>;
 };
 
 export default Element;
